6f76ed4e3c31df315333f46f80e7c22a845bc797
Diego Bonavida <gecko.bona@gmail.com>
2020-03-20 23:39:22 +0100

    Refactor main layout

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+/** Styles */
+import './Footer.scss';
 
-const Footer = () => <div className="layout__footer" />;
+const Footer = () => (
+  <div className="footer__container">
+    <span className="footer__author">bonavida</span>
+    <span className="footer__year">{new Date().getFullYear()}</span>
+  </div>
+);
 
 export default Footer;
--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+/** Styles */
+import './Header.scss';
 
-const Header = () => <div className="layout__header" />;
+const Header = () => (
+  <div className="header__container">
+    <h1 className="header__title">lastfm Collage</h1>
+  </div>
+);
 
 export default Header;
