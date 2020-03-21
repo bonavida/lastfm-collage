4456358df9f42df002efdea4303d158c17992ee6
Diego Bonavida <gecko.bona@gmail.com>
2020-03-21 18:53:14 +0100

    First approach login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -11,6 +11,19 @@ const Login = () => {
 
   return (
     <div className="login__container">
+      <span className="login__description">
+        <p>
+          <span className="login__description--bold">Last.fm Collage</span> is
+          an application built with React and Electron that creates a collage
+          with the cover art of your top favourite albums registered in your
+          last.fm account.
+        </p>
+        <p>
+          No personal information is used or stored. You only need to
+          authenticate and give reading permissions with your last.fm account
+          clicking in the button below.
+        </p>
+      </span>
       <Button className="login__button" onClick={() => handleLogin()}>
         Login to Last.fm
       </Button>
